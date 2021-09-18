a38ad56e34e999452572ff5e3ecb992c54623c92
JANET210995 <janetiridianvs@gmail.com>
2021-09-18 00:33:03 -0500

    1.2

---
+++ src/java/dto/Alumno.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dto;
+
+/**
+ *
+ * @author janet
+ */
+public class Alumno {
+
+    public int id;
+    public int id_materia;
+    public String username;
+    public String nombre;
+    public String apellido_paterno;
+    public String apellido_materno;
+    public String status;
+
+}
---
+++ src/java/dto/Materia.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dto;
+
+/**
+ *
+ * @author janet
+ */
+public class Materia {
+    public int id;
+    public int id_grupo;
+    public String nombre;
+    public String status;
+    
+}
